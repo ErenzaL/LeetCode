70878535e9bca78b4ea8203c8f26402f04b25cc9
Meu <erenza1106@gmail.com>
2025-08-13 18:24:21 +0900

    Time: 268 ms (89.22%), Space: 67 MB (84.13%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
     df = views.loc[views['author_id'] == views['viewer_id']]
-    df = df.drop_duplicates(subset=['author_id','viewer_id']).sort_values(by='author_id')
-    df = df[['author_id']].rename(columns= {'author_id' : 'id'})
-    return df
+    unique_authors = df['author_id'].unique()
+    unique_authors = sorted(unique_authors)
+    result = pd.DataFrame({'id': unique_authors})
+    return result
