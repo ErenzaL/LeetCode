d3590dccfa596ebce6f115f5e0e5566d7b8049f4
Meu <erenza1106@gmail.com>
2025-08-13 18:43:43 +0900

    Time: 277 ms (76.18%), Space: 67.2 MB (66.55%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import numpy as np
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    df = views.loc[views['author_id'] == views['viewer_id']]
-    unique_authors = df['author_id'].unique()
-    unique_authors = sorted(unique_authors)
-    result = pd.DataFrame({'id': unique_authors})
+    df = views[views['author_id'] == views['viewer_id']]
+    authors = np.sort(df['author_id'].unique())
+    result = pd.DataFrame({'id': authors})
     return result
+    
