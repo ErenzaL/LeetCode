8306844613e16b06f23be8972a57023c530ed1a3
Meu <erenza1106@gmail.com>
2025-08-14 13:52:44 +0900

    Time: 312 ms (69.74%), Space: 67.9 MB (46.06%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] =[
-    0 if (eid % 2 == 0 or name.startswith('M')) else salary
-    for eid, name, salary in zip(employees['employee_id'], employees['name'], employees['salary'])]
-    employees = employees[['employee_id', 'bonus']].sort_values(by ='employee_id')
+    even_id = employees['employee_id'] % 2 == 0
+    name_starts_with_m = employees['name'].str.startswith('M')
+    employees['bonus'] = employees['salary'].where(~(even_id | name_starts_with_m), 0)
+    employees = employees[['employee_id', 'bonus']].sort_values(by= 'employee_id')
+
     return employees
-    
