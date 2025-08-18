79b1bb5abc190048ea952cd461768c6aa0e55aec
Meu <erenza1106@gmail.com>
2025-08-18 13:49:21 +0900

    Time: 290 ms (65.63%), Space: 67.6 MB (26.21%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.py
+++ 1827-invalid-tweets/1827-invalid-tweets.py
@@ -2,6 +2,4 @@ import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     invalid = tweets[tweets['content'].str.len() > 15]
-    result = invalid[['tweet_id']]
-    return result
-    
+    return invalid[['tweet_id']]
