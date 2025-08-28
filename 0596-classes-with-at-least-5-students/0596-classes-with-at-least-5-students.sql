ae6907d28ff4e4667a9a1dc7a32d5f9f3ec7f281
Meu <erenza1106@gmail.com>
2025-08-28 17:59:36 +0900

    Time: 356 ms (25.5%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT c1.class
-FROM Courses AS c1
-WHERE (
-    SELECT COUNT(*)
-    FROM Courses as c2
-    WHERE c1.class = c2.class
-    GROUP BY class
-) >=5
+SELECT class
+FROM Courses 
+GROUP BY class
+HAVING COUNT(*) >= 5
